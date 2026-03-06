a1400ae1237c3ea1275a419ce4f4b5601f64a9f1
Jake Strawn <jake.strawn@gmail.com>
2026-03-06 16:06:42 -0500

    refactor: Deep Audit v2: hx-progress-bar

--- packages/hx-library/src/components/hx-progress-bar/hx-progress-bar.styles.ts
+++ packages/hx-library/src/components/hx-progress-bar/hx-progress-bar.styles.ts
@@ -94,4 +94,16 @@ export const helixProgressBarStyles = css`
       opacity: 0.4;
     }
   }
+
+  .progress-bar__live {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border: 0;
+  }
 `;
