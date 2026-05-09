b3254559be871cd4c2eb5bb64ca10bbb36aac0dd
Jake Strawn <jake.strawn@gmail.com>
2026-05-09 17:29:38 -0400

    feat(hx-file-upload): migrate inline svgs to hx-icon library=helix
    Signed-off-by: Jake Strawn <jake.strawn@gmail.com>

--- packages/hx-library/src/components/hx-file-upload/hx-file-upload.ts
+++ packages/hx-library/src/components/hx-file-upload/hx-file-upload.ts
@@ -5,6 +5,7 @@ import { HelixElement, createIdCounter } from '../../base/index.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { repeat } from 'lit/directives/repeat.js';
+import '../hx-icon/hx-icon.js';
 import { mixinDelegatesAria } from '../../mixins/index.js';
 import { FormMixin } from '../../mixins/FormMixin.js';
 import { helixFileUploadStyles } from './hx-file-upload.styles.js';
@@ -639,22 +640,12 @@ export class HelixFileUpload extends FormMixin(mixinDelegatesAria(HelixElement))
                   aria-label=${`Remove ${entry.file.name}`}
                   @click=${() => this._handleRemove(index)}
                 >
-                  <svg
-                    width="14"
-                    height="14"
-                    viewBox="0 0 14 14"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                  <hx-icon
+                    class="file-item__remove-glyph"
+                    library="helix"
+                    name="close"
                     aria-hidden="true"
-                    focusable="false"
-                  >
-                    <path
-                      d="M1 1L13 13M13 1L1 13"
-                      stroke="currentColor"
-                      stroke-width="1.75"
-                      stroke-linecap="round"
-                    />
-                  </svg>
+                  ></hx-icon>
                 </button>
               </div>
               <div
